b4f2f8099dbf00d83122fdd3d8384eb51054b023
Coldfate <63556100+Coldfate@users.noreply.github.com>
2020-11-02 12:34:48 -0600

    Add files via upload

--- Actividad_8.0/src/Deck.java
+++ Actividad_8.0/src/Deck.java
@@ -1,17 +1,24 @@
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.Stack;
 
 public class Deck {
     ArrayList<Object> valores = new ArrayList<>();
     ArrayList<Object> palos = new ArrayList<>();
+    ArrayList<Object> color = new ArrayList<>();
+    Stack<Integer> cards = new Stack<>();
 
     public Deck() {
         createDeck();
     }
-
-    public void createDeck(){
-        for (int i = 2 ; i <= 10 ; i++){
+    public void createDeck() {
+        for (int i = 2; i <= 10; i++) {
             valores.add(i);
         }
+        for (int i = 0 ; i<= 52 ; i++){
+            cards.push(i);
+        }
+
         valores.add("A");
         valores.add("J");
         valores.add("Q");
@@ -20,30 +27,52 @@ public class Deck {
         palos.add("Corazones");
         palos.add("Picas");
         palos.add("Diamantes");
+        color.add("Rojo");
+        color.add("Negro");
 
-        ArrayList cards = new ArrayList<Card>();
-        /*
-        for (int j = 0 ; j <= 4 ; j++) {
-            for (int i = 0 ;i <= 52 ; i++ ){
-                cards.add(new Card(valores.get(i), palos.get(j)));
-            }
-        }
-
-         */
     }
 
     public void shuffle(){
         System.out.println("Se mezcló el deck");
     }
 
-    public void head () {
-
+    public void head() throws Exception{
+        Random rd = new Random();
+        System.out.print(palos.get(rd.nextInt(color.size())) + " ");
+        System.out.print(color.get(rd.nextInt(color.size())) + " ");
+        Object index   = valores.get(rd.nextInt(valores.size()));
+        System.out.println(index + " \n");
+        System.out.println("Quedan " + cards.pop() + " cartas");
+        if (cards.isEmpty()){
+            throw new Exception("Se han agotado las cartas");
+        }
     }
 
-    public void pick () {
-
+    public void pick() throws Exception {
+        Random rd = new Random();
+        System.out.print(palos.get(rd.nextInt(color.size())) + " ");
+        System.out.print(color.get(rd.nextInt(color.size())) + " ");
+        Object index  = valores.get(rd.nextInt(valores.size()));
+        System.out.println(index + " \n");
+        cards.pop();
+        System.out.println("Quedan " + cards.size() + " cartas");
+        if (cards.isEmpty()){
+            throw new Exception("Se han agotado las cartas");
+        }
     }
-    public void hand() {
-
+    public void hand() throws Exception{
+        Random rd = new Random();
+        for (int i = 1; i <= 5 ; i++) {
+            System.out.print(i + " ");
+            System.out.print(palos.get(rd.nextInt(color.size())) + " ");
+            System.out.print(color.get(rd.nextInt(color.size())) + " ");
+            Object index  = valores.get(rd.nextInt(valores.size()));
+            System.out.println(index + "");
+            cards.pop();
+        }
+        System.out.println("Quedan " + cards.size() + " cartas");
+        if (cards.isEmpty()){
+            throw new Exception("Se han agotado las cartas");
+        }
     }
 }
--- Actividad_8.0/src/Main.java
+++ Actividad_8.0/src/Main.java
@@ -1,51 +1,53 @@
-import javax.crypto.spec.DESedeKeySpec;
-import java.util.ArrayList;
 import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         boolean repeat = false;
+
         Scanner scan = new Scanner(System.in);
-        do {
+        try {
             showMenu();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        do {
             System.out.println("Seleccionar otra opción? (s/n)");
             String res = scan.next();
             switch (res) {
-                case "s":
-                    repeat = false;
-                    break;
-                case "n":
-                    repeat = true;
-                    break;
-                default:
-                    System.out.println("Selecciona una opcion valida");
+                case "s" -> showMenu();
+                case "n" -> repeat = true;
+                default -> System.out.println("Selecciona una opcion valida");
             }
-        }while(!repeat);
+        }while (!repeat);
     }
 
-    public static int showMenu() {
+    public static void showMenu() throws Exception {
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Bienvenido a Poker!\nSelecciona una opción:\n1 Mezclar deck\n2 Sacar una carta\n3 Carta al azar\n" +
                 "4 Generar una mano de 5 cartas\n0 Salir");
 
         int res = sc.nextInt();
+
         switch (res) {
-            case 1:
+            case 1 -> {
                 Deck shuffle = new Deck();
                 shuffle.shuffle();
-                break;
-            case 2:
+            }
+            case 2 -> {
                 Deck head = new Deck();
                 head.head();
-                break;
-            case 3:
+            }
+            case 3 -> {
                 Deck pick = new Deck();
                 pick.pick();
-                break;
-            case 4:
+            }
+            case 4 -> {
                 Deck hand = new Deck();
                 hand.hand();
-                break;
+            }
+            case 0 -> System.exit(0);
+            default -> throw new Exception("Opción no valida");
         }
-        return res;
+
     }
 }
